cab9461fb4d1d8897b2bd5da265d7f805684b1de
Haeun Hong <hhe6284@e-mirim.hs.kr>
2017-03-28 20:58:30 +0900

    #2 버튼 누르면 폭파

--- app/src/main/java/kr/hs/emirim/hhe6284/hellogithub/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/hhe6284/hellogithub/MainActivity.java
@@ -2,12 +2,22 @@ package kr.hs.emirim.hhe6284.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button)findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"콰고가ㅗ가과광",Toast.LENGTH_SHORT).show();
     }
 }
